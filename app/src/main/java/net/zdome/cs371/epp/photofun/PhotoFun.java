0eb98cead8bb91a7eb2cfea32b5b441e96cdc230
edcepp <epp@zdome.net>
2017-02-08 11:31:24 -0800

    Initial commit

---
+++ app/src/main/java/net/zdome/cs371/epp/photofun/PhotoFun.java
@@ -0,0 +1,13 @@
+package net.zdome.cs371.epp.photofun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoFun extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_fun);
+    }
+}
